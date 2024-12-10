9ce9541628f92af22cda676d9d5aa271b6cdd185
ZbonfanteZ <zoe.bonfante28@student.tamdistrict.org>
2024-12-10 13:22:01 -0800

    button rerun

--- homework.js
+++ homework.js
@@ -1,15 +1,15 @@
 let currentInterview = 1
-let interview1El = document.getElementById
-let interview2El = document.getElementById
-function switchInterview (){
-if (currentInterview ==1)
-{
-interview1El.hidden = true
-interview2El.hidden = false
-currentInterview = 2
-}
-} else { if (currentInterview ==1)
-    interview2El.hidden = true
-    interview1El.hidden = false
-    currentInterview = 1
+let interview1El = document.getElementById("interview1")
+let interview2El = document.getElementById("interview2")
+function switchInterview() {
+    if (currentInterview == 1) {
+        interview1El.hidden = true
+        interview2El.hidden = false
+        currentInterview = 2
+    }
+    else {
+        interview2El.hidden = true
+        interview1El.hidden = false
+        currentInterview = 1
+    }
 }
